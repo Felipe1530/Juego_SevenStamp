afbe54729ab57e62fddba1c13461d023d05d8809
Felipe_1530 <andresfag@unicauca.edu.co>
2019-07-10 22:59:11 -0500

    Se sube movimientos del personaje

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+   public float smoothing = 5f;
+   Vector3 offset;
+
+   void Start()
+       {
+           offset = transform.position - target.position;// quitamos la posision de objeto a seguir
+       }
+
+       void FixedUpdate()//Generamos que la camara se mueva
+       {
+           Vector3 targetCamPos = target.position + offset;//posicion de destino donde va estar la camara
+           transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+       }
+   
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;//Velocidad de movimiento
+
+    Vector3 movement; //Hacia donde se quiere avanzar las taclas que se usan
+    Animator anim;//Referencia hacia los componentes
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;//La longitug de lo que lanza la arma
+
+    void Awake()//obtener las referencias de los objetos componentes
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+
+    }
+
+    void FixedUpdate()//SE ejecuta para ciclo del motor de fisica
+    {
+        float h = Input.GetAxisRaw("Horizontal");//Se pulza tecla izq o der
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h , v);
+        Turning();
+        animating(h , v);
+
+    }
+    void Move(float h , float v)//Metodo para conocer el movimiento en h y v
+    {
+        movement.Set(h, 0f, v);
+        movement = (movement.normalized * speed * Time.deltaTime);//Se normaliza el vector
+        playerRigidbody.MovePosition(transform.position + movement);//Ya podemos mover el personaje
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);//Se define el giro del personaje atacar
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;//Vector desde la pos del pers hasta donde se toco con el mouse
+            playerToMouse.y = 0f; //Evitamos que el personaje no mire hacia abajo ya que no es necesario
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);//Esto se genera proque unity no entiende sobre vectores
+            //haciendo rotaciones concretas evitando movimientos no deseados
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+    void animating (float h, float v)
+    {
+        bool idle = ((v == 0) && (h == 0));
+        anim.SetBool("IsWalking", !idle);
+    }
+
+    
+}
